acf7135de8a04c9e8b4f1c43781b06bfcbf8f4b7
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-09-21 21:53:20 +0100

    Update Uses.js

--- src/pages/uses/Uses.js
+++ src/pages/uses/Uses.js
@@ -1,5 +1,4 @@
 import { Footer } from 'components/Footer';
-import { Link } from 'components/Link';
 import { List, ListItem } from 'components/List';
 import { Meta } from 'components/Meta';
 import {
@@ -23,25 +22,28 @@ export const Uses = () => {
   return (
     <Fragment>
       <Meta
-        title="Tools"
-        description="A list of hardware and software I use to do my thing"
+        title="Skills"
+        description="Some of the skills I possess."
       />
       <ProjectContainer className={styles.uses}>
-        <DisplacementSphere/>
+        <DisplacementSphere />
         <ProjectHeader
-          title="Uses"
+          title="Skills"
           description="A somewhat comprehensive list of tools, apps and more that I use on a daily basis to design, code and manage things."
         />
         <ProjectSection padding="none" className={styles.section}>
           <ProjectSectionContent>
             <ProjectTextRow width="m">
-              <ProjectSectionHeading>Design</ProjectSectionHeading>
+              <ProjectSectionHeading>Business</ProjectSectionHeading>
               <ProjectSectionText as="div">
                 <List>
-                  <ListItem>
-                    <Link href="https://www.figma.com">Figma</Link> is my primary tool for
-                    UI design these days.
-                  </ListItem>
+                  <ListItem>Stakeholder Management</ListItem>
+                  <ListItem>Strategy</ListItem>
+                  <ListItem>Communication</ListItem>
+                  <ListItem>Agile Project Management</ListItem>
+                  <ListItem>Product Management</ListItem>
+                  <ListItem>Project planning</ListItem>
+                  <ListItem>Scrum</ListItem>
                 </List>
               </ProjectSectionText>
             </ProjectTextRow>
@@ -50,42 +52,36 @@ export const Uses = () => {
         <ProjectSection padding="none" className={styles.section}>
           <ProjectSectionContent>
             <ProjectTextRow width="m">
-              <ProjectSectionHeading>Development</ProjectSectionHeading>
+              <ProjectSectionHeading>Technical skills/tools</ProjectSectionHeading>
               <ProjectSectionText as="div">
                 <List>
-                  <ListItem>
-                    I use{' '}
-                    <Link href="https://code.visualstudio.com/">Visual Studio Code</Link>{' '}
-                    as my text editor, with the Atom One Dark theme and Operator Mono as
-                    my typeface of choice.
-                  </ListItem>
-                  <ListItem>
-                    <Link href="https://reactjs.org/">React</Link> is my front end
-                    Javascript library of choice. The component-centric mental model is
-                    the first thing that truly made sense to me as a designer.
-                  </ListItem>
-                  <ListItem>
-                    For 3D effects and image shaders I use{' '}
-                    <Link href="https://threejs.org/">three.js</Link>. It has a bit of a
-                    learning curve but you can do some really powerful stuff with it.
-                  </ListItem>
-                  <ListItem>
-                    For CSS I’ve used a myriad pre-processors and css-in-js solutions like
-                    styled-components, but these days I’m using vanilla CSS with{' '}
-                    <Link href="https://postcss.org/">PostCSS</Link> to get upcoming CSS
-                    features today.
-                  </ListItem>
-                  <ListItem>
-                    For Javascript animations I use{' '}
-                    <Link href="https://www.framer.com/motion/">Framer Motion</Link>, it’s
-                    a great way to add spring animations to React and three.js.
-                  </ListItem>
+                  <ListItem>Data Analysis</ListItem>
+                  <ListItem>Figma and balsamic prototype/ wireframe designing</ListItem>
+                  <ListItem>Confluence</ListItem>
+                  <ListItem>Jira</ListItem>
+                  <ListItem>Python Programming</ListItem>
+                  <ListItem>Microsoft Office (Excel/Powerpoint)</ListItem>
+                  <ListItem>Power BI</ListItem>
+                  <ListItem>Visio</ListItem>
                 </List>
               </ProjectSectionText>
             </ProjectTextRow>
           </ProjectSectionContent>
         </ProjectSection>
       </ProjectContainer>
+      <ProjectSection padding="none" className={styles.section}>
+        <ProjectSectionContent>
+          <ProjectTextRow width="m">
+            <ProjectSectionHeading>Others</ProjectSectionHeading>
+            <ProjectSectionText as="div">
+              <List>
+                <ListItem>Written communication (check out my medium blogs)</ListItem>
+                <ListItem>Logo Designing (check out my Behance portfolio)</ListItem>
+              </List>
+            </ProjectSectionText>
+          </ProjectTextRow>
+        </ProjectSectionContent>
+      </ProjectSection>
       <Footer />
     </Fragment>
   );
